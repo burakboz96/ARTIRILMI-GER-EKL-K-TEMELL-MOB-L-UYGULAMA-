04e24fca50277c253789d140f081217df886d90b
burak bozoğlu <burakbozoglu96@gmail.com>
2025-04-20 15:22:38 +0300

    Add files via upload

---
+++ ArX Game/ArX Game macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  ArX Game macOS
+//
+//  Created by Burak Bozoğlu on 16.04.2025.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
